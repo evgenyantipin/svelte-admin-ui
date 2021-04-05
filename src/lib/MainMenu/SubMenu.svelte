b245ad7f615a6f95bf82829bea4729428b780197
Mattias Holmlund <mattias@holmlund.se>
2021-04-05 17:52:19 +0200

    Font-awesome chevron in menu

--- src/lib/MainMenu/SubMenu.svelte
+++ src/lib/MainMenu/SubMenu.svelte
@@ -1,10 +1,27 @@
 <script>
+    import Fa from 'svelte-fa'
+    import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
     import { slide } from 'svelte/transition'
+    import { tweened } from 'svelte/motion';
+	import { cubicOut } from 'svelte/easing';
+
+	const rotation = tweened(0, {
+		duration: 400,
+		easing: cubicOut
+	});
+
     export let title
     export let show = false
 </script>
 
-<p class="cursor-pointer" on:click={() => (show = !show)}>{title}</p>
+<p class="cursor-pointer" on:click={() => {
+    show = !show;
+    if(show) { 
+        rotation.set(0) 
+    } else {
+        rotation.set(180)
+    }
+  }}>{title} <Fa icon={faChevronUp} style="display: inline-block; margin-right: 3px; margin-top: 2px;" pull="right" rotate={$rotation}></Fa></p>
 {#if show}
 <div class="overflow-hidden" transition:slide={{ duration: 200 }}>
     <ul class="menu-list" >
