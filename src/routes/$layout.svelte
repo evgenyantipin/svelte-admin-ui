022b5e03b9922a0efd32ff3f2de267d9f3a80dee
Mattias Holmlund <mattias@holmlund.se>
2021-04-11 16:31:45 +0200

    Fix bug where spinner was covering content
    Even when it was not shown

--- src/routes/$layout.svelte
+++ src/routes/$layout.svelte
@@ -30,13 +30,15 @@ import Index from "./peopleLoad/index.svelte";
 		<!-- Main -->
 		<div class="flex-1 flex">
 			<div class="px-6 py-4 flex-1 overflow-y-auto relative">
-				<div style="opacity: {$navigating ? 100 : 0};" class="absolute top-0 left-0 w-full h-full flex justify-center content-end p-10 transition-opacity duration-300 delay-200 ease-in">
+				<div style="opacity: {$navigating ? 100 : 0}; display: {$navigating ? 'block' : 'none'};" class="absolute top-0 left-0 w-full h-full flex justify-center content-end p-10 transition-opacity duration-300 delay-200 ease-in">
 					<svg style="opacity: {$navigating ? 100 : 0};" class="animate-spin h-full w-full" fill="none" viewBox="0 0 24 24">
 						<circle class="opacity-10" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
 						<path class="opacity-50" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
 					</svg>	
 				</div>
-				<slot/>
+				<div class="z-10">
+					<slot/>
+				</div>
 		
 			</div>
 		</div>
